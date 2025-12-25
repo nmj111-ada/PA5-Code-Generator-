2701240bb37fb178c2e103d1130efef52e47acc1
nmj111-ada <a2585715671@163.com>
2025-12-25 16:46:34 +0800

    Update cool-tree.handcode.h

--- cool-tree.handcode.h
+++ cool-tree.handcode.h
@@ -1,58 +1,136 @@
-#define Expression_EXTRAS                    \
-    Symbol type;                             \
-    Symbol get_type() { return type; }       \
+#ifndef COOL_TREE_HANDCODE_H
+#define COOL_TREE_HANDCODE_H
+
+#include "cool-tree.h"
+#include "cgen.h"
+/* ============================================================
+   Expression extras（PA5 标准）
+   ============================================================ */
+
+#define Expression_EXTRAS                             \
+    Symbol type;                                      \
+    Symbol get_type() { return type; }                \
     Expression set_type(Symbol s) { type = s; return this; } \
-    virtual void code(ostream&, Environment) = 0; \
+    virtual void code(ostream&, Environment) = 0;     \
     virtual bool IsEmpty() { return false; }
+
+/* ============================================================
+   所有 Expression 节点的 code() 声明
+   ============================================================ */
+
+/* ---------- constants ---------- */
+
+class int_const_class : public Expression {
+public:
+    Symbol token;
+    void code(ostream&, Environment);
+};
+
+class string_const_class : public Expression {
+public:
+    Symbol token;
+    void code(ostream&, Environment);
+};
+
+class bool_const_class : public Expression {
+public:
+    bool val;
+    void code(ostream&, Environment);
+};
+
+/* ---------- variables ---------- */
+
+class object_class : public Expression {
+public:
+    Symbol name;
+    void code(ostream&, Environment);
+};
 
 class assign_class : public Expression {
 public:
     Symbol name;
     Expression expr;
-
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
+
+/* ---------- arithmetic ---------- */
 
 class plus_class : public Expression {
 public:
     Expression e1, e2;
-
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
 
 class sub_class : public Expression {
 public:
     Expression e1, e2;
-
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
 
 class mul_class : public Expression {
 public:
     Expression e1, e2;
-
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
 
 class divide_class : public Expression {
 public:
     Expression e1, e2;
-
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
 
-class object_class : public Expression {
+/* ---------- comparison ---------- */
+
+class lt_class : public Expression {
 public:
-    Symbol name;
-
+    Expression e1, e2;
     void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
 };
+
+class leq_class : public Expression {
+public:
+    Expression e1, e2;
+    void code(ostream&, Environment);
+};
+
+class eq_class : public Expression {
+public:
+    Expression e1, e2;
+    void code(ostream&, Environment);
+};
+
+/* ---------- control flow ---------- */
+
+class cond_class : public Expression {
+public:
+    Expression pred, then_exp, else_exp;
+    void code(ostream&, Environment);
+};
+
+class loop_class : public Expression {
+public:
+    Expression pred, body;
+    void code(ostream&, Environment);
+};
+
+class block_class : public Expression {
+public:
+    Expressions body;
+    void code(ostream&, Environment);
+};
+
+/* ---------- let ---------- */
+
+class let_class : public Expression {
+public:
+    Symbol identifier;
+    Symbol type_decl;
+    Expression init;
+    Expression body;
+    void code(ostream&, Environment);
+};
+
+/* ---------- dispatch ---------- */
 
 class dispatch_class : public Expression {
 public:
@@ -61,100 +139,20 @@ public:
 
     void code(ostream&, Environment);
     vector<Expression*> GetActuals();
-    bool IsEmpty() { return false; }
 };
 
 class static_dispatch_class : public Expression {
 public:
     Expression expr;
+    Symbol type_name;
     Expressions actual;
 
     void code(ostream&, Environment);
     vector<Expression*> GetActuals();
-    bool IsEmpty() { return false; }
 };
 
-class cond_class : public Expression {
-public:
-    Expression pred, then_exp, else_exp;
+/* ---------- case ---------- */
 
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class loop_class : public Expression {
-public:
-    Expression pred, body;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class block_class : public Expression {
-public:
-    Expressions body;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class let_class : public Expression {
-public:
-    Symbol identifier;
-    Symbol type_decl;
-    Expression init;
-    Expression body;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class case_class : public Expression {
-public:
-    Expression expr;
-    Symbol type_decl;
-    Symbol name;
-    Expression branch;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class typcase_class : public Expression {
-public:
-    Expression expr;
-    Cases cases;
-
-    void code(ostream&, Environment);
-    vector<branch_class*> GetCases();
-    bool IsEmpty() { return false; }
-};
-
-class new__class : public Expression {
-public:
-    Symbol type_name;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class isvoid_class : public Expression {
-public:
-    Expression e;
-
-    void code(ostream&, Environment);
-    bool IsEmpty() { return false; }
-};
-
-class no_expr_class : public Expression {
-public:
-    void code(ostream&, Environment) {
-        // 永远不会被调用
-    }
-    bool IsEmpty() { return true; }
-};
-
-#branch_class 补充
 class branch_class : public tree_node {
 public:
     Symbol name;
@@ -166,69 +164,34 @@ public:
     Expression GetExpr() { return expr; }
 };
 
-#formal_class 补充
-class formal_class : public tree_node {
+class typcase_class : public Expression {
 public:
-    Symbol name;
-    Symbol type_decl;
-
-    Symbol GetName() { return name; }
-    Symbol GetTypeDecl() { return type_decl; }
-};
-
-#attr_class 补充
-class attr_class : public Feature {
-public:
-    Symbol name;
-    Symbol type_decl;
-    Expression init;
-
-    Symbol GetName() { return name; }
-    Symbol GetTypeDecl() { return type_decl; }
-    Expression GetInit() { return init; }
-};
-
-#method_class 补充
-class method_class : public Feature {
-public:
-    Symbol name;
-    Formals formals;
-    Symbol return_type;
     Expression expr;
+    Cases cases;
 
-    Symbol GetName() { return name; }
-    Formals GetFormals() { return formals; }
-    Expression GetExpr() { return expr; }
+    void code(ostream&, Environment);
+    vector<branch_class*> GetCases();
 };
 
-#细节补充
-class Expression {
+/* ---------- misc ---------- */
+
+class new__class : public Expression {
 public:
-    virtual void code(ostream& s, Environment env) = 0;
-
-    virtual bool IsEmpty() { return false; }
+    Symbol type_name;
+    void code(ostream&, Environment);
 };
-vector<Expression*> dispatch_class::GetActuals() {
-    vector<Expression*> v;
-    for (int i = actual->first(); actual->more(i); i = actual->next(i)) {
-        v.push_back(actual->nth(i));
-    }
-    return v;
-}
 
-vector<Expression*> static_dispatch_class::GetActuals() {
-    vector<Expression*> v;
-    for (int i = actual->first(); actual->more(i); i = actual->next(i)) {
-        v.push_back(actual->nth(i));
-    }
-    return v;
-}
+class isvoid_class : public Expression {
+public:
+    Expression e;
+    void code(ostream&, Environment);
+};
 
-vector<branch_class*> typcase_class::GetCases() {
-    vector<branch_class*> v;
-    for (int i = cases->first(); cases->more(i); i = cases->next(i)) {
-        v.push_back(cases->nth(i));
-    }
-    return v;
-}
+class no_expr_class : public Expression {
+public:
+    void code(ostream&, Environment) {}
+    bool IsEmpty() { return true; }
+};
+
+#endif
 
